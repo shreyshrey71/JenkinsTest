012df66c199a53b94242a31641ddc5436940bb63
Shrey Garg <shreyshrey71@yahoo.in>
2022-08-02 16:40:22 +0530

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello New");
+        for(int i=0;i<11;i++){
+            System.out.println("Value of I is : "+i);
+        }
+    }
+}
